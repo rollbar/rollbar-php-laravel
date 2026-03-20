bf118ff8a6024f41b55cfef725af2f9ab8b29809
Daniel Morell <daniel@cairntek.com>
2026-03-20 23:43:13 +0100

    Resolved issues in PHPUnit test suite after updating to PHPUnit 10 and above.

--- tests/TelemetryListenerTest.php
+++ tests/TelemetryListenerTest.php
@@ -8,9 +8,15 @@ use Illuminate\Http\Request;
 use Illuminate\Log\Events\MessageLogged;
 use Illuminate\Routing\Events\RouteMatched;
 use Illuminate\Routing\Route;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\UsesClass;
+use Rollbar\Laravel\RollbarServiceProvider;
+use Rollbar\Laravel\TelemetryListener;
 use Rollbar\Rollbar;
 use Rollbar\Telemetry\EventLevel;
 
+#[UsesClass(RollbarServiceProvider::class)]
+#[CoversClass(TelemetryListener::class)]
 class TelemetryListenerTest extends AbstractTestCase
 {
     protected function setUp(): void
